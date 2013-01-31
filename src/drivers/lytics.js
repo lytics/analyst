e36f3ddf284286fcf5aba6eed96ea2227f067a79
Steven Lindberg <stevenjohnlindberg@gmail.com>
2013-01-30 16:43:40 -0800

    Restructured lytics driver, added modifier of top() values to key/value pairs

--- src/drivers/lytics.js
+++ src/drivers/lytics.js
@@ -3,15 +3,11 @@ analyst.addDriver('lytics', function(options) {
 
   options = options || {};
 
-  return function(limit) {
+  function getUrl(options, limit) {
     var baseUrl = options.url || '//api.lytics.io',
       url = baseUrl + '/api/' + (options.clientId ? options.clientId + '/' : '') + options.query,
       data = options.data || {},
       params = [];
-
-    var script = document.createElement('script');
-    var cbName = 'analyst_lytics_' + (new Date()).getTime();
-    params.push('callback=' + cbName);
 
     // Add the rest of the data to the query as params
     Object.keys(data).forEach(function(key) {
@@ -22,62 +18,111 @@ analyst.addDriver('lytics', function(options) {
       url += '?' + params.join('&');
     }
 
-    var handleResponse = function(response) {
-      var data = [];
+    return url;
+  }
 
-      // Extract field indices
-      if (response.meta) {
-        var dimensions = response.meta.dimensions,
-          measures = response.meta.measures,
-          fields = {};
-          offset = 1;
+  function handleResponse(response) {
+    if (response.meta && response.data) {
+      parseData(response.data, parseMeta(response.meta));
+    } else {
+      // TODO: handle abnormal response
+    }
+  }
 
-        // Dimensions come first
-        if (dimensions && dimensions.length > 0) {
-          dimensions.forEach(function(field, index) {
-            fields[field] = index;
-          });
+  function parseMeta(meta) {
+    // Extract field indices
+    var dimensions = meta.dimensions,
+      measures = meta.measures,
+      transforms = {},
+      fields = {},
+      offset = 1;
 
-          offset = dimensions.length;
-        } else {
-          fields._ = 0;
-        }
+    // Dimensions come first
+    if (dimensions && dimensions.length > 0) {
+      dimensions.forEach(function(field, index) {
+        fields[field] = index;
+      });
 
-        // Measures next
-        measures.forEach(function(measure, index) {
-          fields[measure.As] = index + offset;
+      offset = dimensions.length;
+    } else {
+      fields._ = 0;
+    }
+
+    // Measures next
+    measures.forEach(function(measure, index) {
+      fields[measure.As] = index + offset;
+
+      if (measure.Op === 'top') {
+        transforms[index + offset] = convertTop;
+      }
+    });
+
+    // Timestamp last
+    fields._ts = offset + measures.length;
+    fields._date = fields._ts + 1;
+
+    // Set field mapping
+    source.fieldMap(fields);
+
+    return transforms;
+  }
+
+  function parseData(rawData, transforms) {
+    var data = [];
+
+    // Parse data
+    rawData.forEach(function(segment) {
+      var ts = segment._ts,
+        date = new Date(ts.ts * 1000);
+
+      segment.rows.forEach(function(row, index) {
+        // Apply any transforms to the raw data
+        Object.keys(transforms).forEach(function(index) {
+          row[index] = transforms[index](row[index]);
         });
 
-        // Timestamp last
-        fields._ts = offset + measures.length;
-        fields._date = fields._ts + 1;
+        // Add a native JS date and timestamp to the row
+        row.push(ts.ts);
+        row.push(date);
+        data.push(row);
+      });
+    });
 
-        source.fieldMap(fields);
-      }
+    // Add the formatted data to the source
+    source.add(data);
+  }
 
-      // Parse data
-      if (response.data) {
-        response.data.forEach(function(segment) {
-          var ts = segment._ts,
-            date = new Date(ts.ts * 1000);
+  // Convert an array of { <key>: <value> } objects to { key: <key>, value: <value> }
+  function convertTop(arr) {
+    if (!Array.isArray(arr)) {
+      return arr;
+    }
 
-          segment.rows.forEach(function(row, index) {
-            // Add a native JS date and timestamp to the row
-            row.push(ts.ts);
-            row.push(date);
-            data.push(row);
-          });
-        });
-      }
+    return arr.map(function(obj) {
+      key = Object.keys(obj)[0];
+      return {
+        key: key,
+        value: obj[key]
+      };
+    });
+  }
 
-      source.add(data);
+  // TODO: handle timeout of the script request
+  return function(limit) {
+    // Hack this shiz in, old school JSONP it for now
+    var script = document.createElement('script'),
+      cbName = 'analyst_lytics_' + (new Date()).getTime();
+
+    options.data = options.data || {};
+    options.data.callback = cbName;
+
+    root[cbName] = function(response) {
+      handleResponse(response);
       delete root[cbName];
       script.remove();
     };
 
-    // Hack this shiz in, old school JSONP it for now
-    script.src = url;
-    root[cbName] = handleResponse;
+    script.src = getUrl(options, limit);
     document.body.appendChild(script);
   };
 });
