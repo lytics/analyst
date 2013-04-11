26952fba59bae0867a75e1231d6b80bd19cf3670
Steven Lindberg <stevenjohnlindberg@gmail.com>
2013-04-11 14:55:07 -0700

    Updated lytics driver to reflect API changes

--- src/drivers/lytics.js
+++ src/drivers/lytics.js
@@ -5,9 +5,14 @@ analyst.addDriver('lytics', function(options) {
 
   function getUrl(options, limit) {
     var baseUrl = options.url || '//api.lytics.io',
-      url = baseUrl + '/api/' + (options.clientId ? options.clientId + '/' : '') + options.query,
+      url = baseUrl + '/api/t/' + options.query,
       data = options.data || {},
       params = [];
+
+    // Add the client ID if supplied to remove ambiguity
+    if (options.clientId) {
+      data.aid = options.clientId;
+    }
 
     // Add the rest of the data to the query as params
     Object.keys(data).forEach(function(key) {
@@ -22,74 +27,91 @@ analyst.addDriver('lytics', function(options) {
   }
 
   function handleResponse(response) {
-    if (response.meta && response.data) {
-      parseData(response.data, parseMeta(response.meta));
-    } else {
-      // TODO: handle abnormal response
-    }
-  }
+    var transforms = {},
+      rowLength;
 
-  function parseMeta(meta) {
-    // Extract field indices
-    var dimensions = meta.dimensions,
-      measures = meta.measures,
-      transforms = {},
-      fields = {},
-      offset = 1;
+    parseMeta(response.meta);
+    parseData(response.data);
 
-    // Dimensions come first
-    if (dimensions && dimensions.length > 0) {
-      dimensions.forEach(function(field, index) {
-        fields[field] = index;
-      });
-
-      offset = dimensions.length;
-    } else {
-      fields._ = 0;
-    }
-
-    // Measures next
-    measures.forEach(function(measure, index) {
-      fields[measure.As] = index + offset;
-
-      if (measure.Op === 'top') {
-        transforms[index + offset] = convertTop;
+    function parseMeta(meta) {
+      if (!meta) {
+        throwError('missing metadata');
       }
-    });
 
-    // Timestamp last
-    fields._ts = offset + measures.length;
-    fields._date = fields._ts + 1;
+      // Extract field indices
+      var measures = meta.measures,
+        fields = {};
 
-    // Set field mapping
-    source.fieldMap(fields);
+      rowLength = measures.length;
 
-    return transforms;
+      if (!rowLength) {
+        throwError('empty measures meta information');
+      }
+
+      // Measures next
+      measures.forEach(function(measure, index) {
+        if (!measure || !measure.as) {
+          throwError('invalid measure');
+        }
+
+        fields[measure.as] = index;
+
+        if (measure.op === 'top') {
+          transforms[index] = convertTop;
+        }
+      });
+
+      // Timestamp last
+      fields._ts = rowLength;
+      fields._date = rowLength + 1;
+
+      // Set field mapping
+      source.fieldMap(fields);
+    }
+
+    function parseData(rawData) {
+      if (!Array.isArray(rawData)) {
+        throwError('missing data or data is not an array');
+      }
+
+      var data = [];
+
+      // Parse data
+      rawData.forEach(function(dataGroup) {
+        if (!dataGroup._ts || !dataGroup._ts.ts) {
+          throwError('missing or malformed timestamp object');
+        }
+
+        if (!Array.isArray(dataGroup.rows)) {
+          throwError('row data is missing or not an array');
+        }
+
+        var date = new Date(dataGroup._ts.ts * 1000);
+
+        dataGroup.rows.forEach(function(row, index) {
+          if (row.length != rowLength) {
+            throwError('row data length mismatch (got ' + row.length + ', expected ' + rowLength + ')');
+          }
+
+          // Apply any transforms to the raw data
+          Object.keys(transforms).forEach(function(index) {
+            row[index] = transforms[index](row[index]);
+          });
+
+          // Add a native JS date and timestamp to the row
+          row.push(dataGroup._ts);
+          row.push(date);
+          data.push(row);
+        });
+      });
+
+      // Add the formatted data to the source
+      source.add(data);
+    }
   }
 
-  function parseData(rawData, transforms) {
-    var data = [];
-
-    // Parse data
-    rawData.forEach(function(segment) {
-      var ts = segment._ts,
-        date = new Date(ts.ts * 1000);
-
-      segment.rows.forEach(function(row, index) {
-        // Apply any transforms to the raw data
-        Object.keys(transforms).forEach(function(index) {
-          row[index] = transforms[index](row[index]);
-        });
-
-        // Add a native JS date and timestamp to the row
-        row.push(ts.ts);
-        row.push(date);
-        data.push(row);
-      });
-    });
-
-    // Add the formatted data to the source
-    source.add(data);
+  function throwError(message) {
+    throw new Error('Malformed response: ' + message);
   }
 
   // Convert an array of { <key>: <value> } objects to { key: <key>, value: <value> }
